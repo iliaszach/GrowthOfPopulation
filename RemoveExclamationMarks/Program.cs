3116841f44b3b6c1892f8b0a52ca9b7c32f93b60
Ilias <77877193+iliaszach@users.noreply.github.com>
2021-06-25 13:37:11 +0300

    Add project files.

---
+++ RemoveExclamationMarks/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveExclamationMarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NbYear(1000,2,50,1200));
+        }
+
+        public static int NbYear(int p0, double percent, int aug, int p)
+        {            
+            int count = 0;
+            while (p0 < p)
+            {
+                count++;               
+                p0 = Convert.ToInt32(p0 + Math.Floor(p0 * percent / 100) + aug);
+                
+            }     
+            return count;
+           
+        }
+
+    }
+}
